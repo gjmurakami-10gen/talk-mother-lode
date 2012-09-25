856ac262aa14becddf9d82778a9102d4fae5e188
Gary Murakami <gary.murakami@10gen.com>
2012-09-25 09:35:04 -0400

    talk slides - improvements - california gold rush

--- twitter/load_users.rb
+++ twitter/load_users.rb
@@ -10,12 +10,12 @@ connection = Mongo::Connection.new
 db = connection['twitter']
 collection = db['users']
 
-friend_ids.each_slice(100) do |ids|
+friend_ids.each_slice(100) do |ids| # best practices
   users_lookup_uri = "https://api.twitter.com/1/users/lookup.json?user_id=#{ids.join(',')}"
   response = HTTPClient.get(users_lookup_uri)
 
   docs = JSON.parse(response.body) # high-level objects
   docs.each{|doc| doc['_id'] = doc['id']} # user supplied _id
-  collection.insert(docs, :safe => true) # bulk insert
+  collection.insert(docs, :safe => true) # bulk insert - best practices
 end
 puts "users:#{collection.count}"
